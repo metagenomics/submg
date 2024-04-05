56ecdc3b798142866481e392b501b98a0ce261b1
ttubb <t.tubbesing@uni-bielefeld.de>
2024-04-05 13:55:53 +0000

    temp workaround for dev api

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools.command.install import install
 class CustomInstall(install):
     def run(self):
         install.run(self)
-        print("\nIMPORTANT: Please run the 'synum download_webin' command.")
+        print("\nIMPORTANT: Please run the 'submg download_webin' command.")
 
 setup(
-    name='synum',
-    version='0.9.1',
+    name='submg',
+    version='0.9.2',
     packages=find_packages(),
     install_requires=[
         'pysam>=0.19.1',
@@ -20,15 +20,15 @@ setup(
         'yaspin>=2.0',
     ],
     scripts=[
-        'synum/main.py',
+        'submg/main.py',
     ],
     include_package_data=True,
     package_data={
-        'synum': ['*.yaml','*.fa','*.fasta','*.fasta.fai','*.bam','*.bam.bai','*.tsv','*.txt']
+        'submg': ['*.yaml','*.fa','*.fasta','*.fasta.fai','*.bam','*.bam.bai','*.tsv','*.txt']
     },
     entry_points={
         'console_scripts': [
-            'synum=synum.main:main'
+            'submg=submg.main:main'
         ],
     },
     cmdclass={
@@ -39,5 +39,5 @@ setup(
     author_email='t.tubbesing@uni-bielefeld.de',
     description='A tool for submitting metagenome data to ENA',
     keywords='Metagenome, MAG, ENA, Submission',
-    url='https://github.com/ttubb/synum',
+    url='https://github.com/ttubb/submg',
 )
